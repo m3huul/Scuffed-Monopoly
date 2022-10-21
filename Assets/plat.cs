fbd86b6d003718efa3c0066011bbbb1e13de1bc3
m3huul <96343363+m3huul@users.noreply.github.com>
2022-10-21 19:28:30 +0530

    RestAPI Integration

---
+++ Assets/plat.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plat : MonoBehaviour
+{
+    public string name;
+    public string price;
+    public string rent;
+    public string house1;
+    public string house2;
+    public string house3;
+    public string house4;
+    public string price_per_house;
+    public string hotel;
+    public string mortgage;
+
+
+
+}
