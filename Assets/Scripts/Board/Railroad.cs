0df1a098f2a149ce17e0f98ca79c5c316f557dd4
Art3miz <aryansofficial97@gmail.com>
2022-11-09 12:56:46 +0530

    Added URP and board arrangement + build settings

--- Assets/Scripts/Board/Railroad.cs
+++ Assets/Scripts/Board/Railroad.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.VersionControl;
+//using UnityEditor.VersionControl;
 using UnityEngine;
 
 public class Railroad : Ownable
--- Assets/Scripts/Monopoly.cs
+++ Assets/Scripts/Monopoly.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
-using UnityEditor.Purchasing;
+//using UnityEditor.Purchasing;
 
 public class Monopoly : MonoBehaviour
 {
